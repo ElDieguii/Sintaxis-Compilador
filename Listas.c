28c924b92e29521532ff331120e99c7b5ad18360
ElDieguii <49466415+ElDieguii@users.noreply.github.com>
2019-07-09 13:11:47 -0300

    Update Listas.c

--- Listas.c
+++ Listas.c
@@ -1,44 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//------ COMENTARIOS ----------
-typedef struct nodoComentario{
-    char *comentario;
-    struct nodoComentario *sig;
-}comentarios;
-
-comentarios *listaDeComentarios (comentarios *lista){
-    lista = NULL;
-    return lista;
-}
-
-comentarios *listarComentario(comentarios*lista, char *comentario){
-    comentarios *nuevo_comentario, *aux, *aux2;
-    nuevo_comentario = (comentarios*)malloc(sizeof(comentarios));
-    strcpy(nuevo_comentario->comentario, comentario);
-    nuevo_comentario->sig = NULL;
-    aux = lista;
-
-    if(lista==NULL){
-        lista = nuevo_comentario;
-    }
-    else{
-        while(aux !=NULL){
-            aux2=aux;
-            aux=aux->sig;
-
-        }
-        aux2->sig = nuevo_comentario;
-    }
-    return lista;
-}
-
-
-
-
-
-
-
 
 //------- CARACTERES DE PUNTUACION --------------
 typedef struct nodoCaracterPuntuacion{
@@ -81,7 +43,7 @@ caracter_puntuacion *agregarCaracter(caracter_puntuacion *lista, char caracter){
 
 int main(){
     caracter_puntuacion *lista = listaCaracter(lista);
-    comentarios *listaComentarios = listaDeComentarios(listaComentarios); 
+    
 
     ///--- PRUEBA DE CARACTERES ---
     lista = agregarCaracter(lista, 'a');
@@ -98,14 +60,6 @@ int main(){
         lista = lista->sig;
     }
 
-    //--- PRUEBA DE COMENTARIOS ---
-    //listaComentarios = listarComentario(listaComentarios, "asfasf");
-    /* listaComentarios = listarComentario(listaComentarios, "asfasfg");
-    /* while(listaComentarios!=NULL){
-        printf("%s ", listaComentarios->comentario);
-        listaComentarios = listaComentarios->sig;
-    }
-    */
 
     return 0;
 }
